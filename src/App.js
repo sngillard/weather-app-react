426df182e8c72e37f8a256f34942ccd3e83c8c0e
Sarah Gillard <108188646+sngillard@users.noreply.github.com>
2022-12-13 16:29:58 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import React from "react";
 
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="www.linkedin.com/in/sarahgillard3" target="_blank">
+          Sarah Gillard
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/sngillard/weather-app-react"
+        target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
