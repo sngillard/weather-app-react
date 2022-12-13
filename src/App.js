e01083f8aae29205584d68f2d1d1b908aaef1d53
Sarah Gillard <108188646+sngillard@users.noreply.github.com>
2022-12-13 16:43:14 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
@@ -17,6 +18,7 @@ export default function App() {
           open-sourced on Github
         </a>
       </footer>
+      </div>
     </div>
   );
 }
