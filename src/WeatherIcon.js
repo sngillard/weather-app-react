c8725b19013fb174e5cc16e2d67fdd8e0b73e650
Sarah Gillard <108188646+sngillard@users.noreply.github.com>
2022-12-19 16:33:01 -0500

    Troubleshooting SheCodes API with animated react weather icons.

---
+++ src/WeatherIcon.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
+
+export default function WeatherIcon(props) {
+  const codeMapping = {
+    "clear-sky-day": "CLEAR_DAY",
+    "clear-sky-night": "CLEAR_NIGHT",
+    "few-clouds-day": "PARTLY_CLOUDY_DAY",
+    "few-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "scattered-clouds-day": "PARTLY_CLOUDY_NIGHT",
+    "scattered-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "broken-clouds-day": "CLOUDY_DAY",
+    "broken-clouds-night": "CLOUDY_NIGHT",
+    "shower-rain-day": "RAIN",
+    "shower-rain-night": "RAIN",
+    "rain-day": "RAIN",
+    "rain-night": "RAIN",
+    "thunderstorm-day": "RAIN",
+    "thunderstorm-night": "RAIN",
+    "snow-day": "SNOW",
+    "snow-night": "SNOW",
+    "mist-day": "FOG",
+    "mist-night": "FOG",
+  };
+
+  return (
+    <ReactAnimatedWeather
+      icon={codeMapping[props.code]}
+      color="black"
+      size={52}
+      animate={true}
+    />
+  );
+}
