87c12cf64fb711061e47684b5e3aedf2f995a00b
Sarah Gillard <108188646+sngillard@users.noreply.github.com>
2022-12-21 11:56:39 -0500

    Adjusted css for screens max-width 450px and max-width 280px.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,7 +17,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix d-flex align-items-center">
             <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={45} />
+              <WeatherIcon code={props.data.icon} size={56} />
             </div>
 
             <div className="float-left">
